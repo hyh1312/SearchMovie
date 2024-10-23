d4a5750eb5567a4e5e5990f6d4adbd812a693e06
hyh1312 <hyh1312@126.com>
2024-10-24 00:32:06 +0800

    重构OnlineSearchUtil+实现searchDetails

--- app/src/main/java/com/example/myapplication/network/OnlineSearchUtil.java
+++ app/src/main/java/com/example/myapplication/network/OnlineSearchUtil.java
@@ -14,14 +14,11 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class OnlineSearchUtil {
-    private static String URL = "http://www.omdbapi.com/?apikey=858fc655";
+    private static final String URL = "http://www.omdbapi.com/?apikey=858fc655";
 
-    public static void searchMovies(String query, Callback callback) {
+    public static void search(String query, Callback callback) {
         OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(URL + "&s=" + query) // + "&page=" + page 实现下一页（还没写）
-                .get()
-                .build();
+        Request request = new Request.Builder().url(query).get().build();
         Call call = client.newCall(request);
         call.enqueue(new Callback() {
             @Override
@@ -29,6 +26,7 @@ public class OnlineSearchUtil {
                 Log.d("--------Fail-------", "onFailure: " + e.toString());
                 callback.onFailure(call, e);
             }
+
             @SuppressLint("NotifyDataSetChanged")
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
@@ -37,26 +35,11 @@ public class OnlineSearchUtil {
         });
     }
 
-    public static void getDetail(String query, Callback callback) {
+    public static void searchMovies(String name, Callback callback) {
+        search(URL + "&s=" + name, callback); // + "&page=" + page 实现下一页（还没写）
+    }
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(URL + "&t=" + query) // query 是 id
-                .get()
-                .build();
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                Log.d("--------Fail-------", "onFailure: " + e.toString());
-                callback.onFailure(call, e);
-            }
-
-            @SuppressLint("NotifyDataSetChanged")
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                callback.onResponse(call, response);
-            }
-        });
+    public static void searchDetails(String id, Callback callback) {
+        search(URL + "&t=" + id, callback);
     }
 }
