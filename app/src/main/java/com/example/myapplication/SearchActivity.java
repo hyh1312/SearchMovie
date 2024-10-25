311bf890554ea0a4381050a661911de101aaeb26
hyh1312 <hyh1312@126.com>
2024-10-25 16:17:39 +0800

    重写翻页，修复bug

--- app/src/main/java/com/example/myapplication/SearchActivity.java
+++ app/src/main/java/com/example/myapplication/SearchActivity.java
@@ -1,14 +1,13 @@
 package com.example.myapplication;
 
+import static java.lang.Thread.sleep;
+
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -32,83 +31,28 @@ public class SearchActivity extends AppCompatActivity {
     private MovieAdapter movieAdapter;
     private ProgressBar progressBar;
     private List<Movie> movieList = ListModel.movieList;
-    private int page = 1;
+    private String query;
     private EditText editText;
-
+    private Button previousButton, nextButton;
+    private int page = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_search);
-
-        String query = getIntent().getStringExtra("query");
-
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+        query = getIntent().getStringExtra("query");
         movieAdapter = new MovieAdapter(movieList, this);
         progressBar = findViewById(R.id.loading_spinner);
         progressBar.setVisibility(View.VISIBLE);
-        Button previousButton = findViewById(R.id.btnPrevious);
-        Button nextButton = findViewById(R.id.btnNext);
-
-
+        previousButton = findViewById(R.id.btnPrevious);
+        nextButton = findViewById(R.id.btnNext);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(movieAdapter);
 
-        Callback callback = new Callback() {
-            // private boolean responseFlag=false;
-            // public boolean isResponse(){ return responseFlag; }
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                runOnUiThread(() -> {
-                    Toast.makeText(SearchActivity.this, "请求失败", Toast.LENGTH_SHORT).show();
-                    progressBar.setVisibility(View.GONE);
-                });
-            }
+        tryChangePage(1);
 
-            @SuppressLint("NotifyDataSetChanged")
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) {
-                try {
-                    movieList = SearchViewModel.addList(response);
-                    // responseFlag=true;
-                    runOnUiThread(() -> {
-                        movieAdapter.notifyDataSetChanged();
-                        progressBar.setVisibility(View.GONE);
-                    });
-                } catch (Exception e) {
-                    runOnUiThread(() -> {
-                        Toast.makeText(SearchActivity.this, "未找到电影", Toast.LENGTH_SHORT).show();
-                        progressBar.setVisibility(View.GONE);
-                    });
-                }
-            }
-        };
-
-        OnlineSearchUtil.searchMovies(query, callback);
-
-        page = 1;
-        previousButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (page > 1) {
-                    OnlineSearchUtil.searchMoviesByPage(query, page-1, callback);
-                    if(true){ // callback.isResponse ?
-                        page--;
-                    }
-                }
-            }
-        });
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (page < 100) { // maxPage?
-                    OnlineSearchUtil.searchMoviesByPage(query, page+1, callback);
-                    if(true){ // callback.isResponse ?
-                        page++;
-                    }
-                }
-            }
-        });
         /*
         editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -121,9 +65,22 @@ public class SearchActivity extends AppCompatActivity {
                 return false;
             }
         });
-*/
-    }
+        */
 
+        previousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tryChangePage(page - 1);
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tryChangePage(page + 1);
+            }
+        });
+    }
 
     @SuppressLint("NotifyDataSetChanged")
     @Override
@@ -131,6 +88,42 @@ public class SearchActivity extends AppCompatActivity {
         super.onDestroy();
         movieList.clear();
         movieAdapter.notifyDataSetChanged();
+    }
+
+    private void tryChangePage(int newPage) {
+        loading(true);
+        OnlineSearchUtil.searchMoviesByPage(query, newPage, new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                runOnUiThread(() -> {
+                    Toast.makeText(SearchActivity.this, "请求失败", Toast.LENGTH_SHORT).show();
+                    loading(false);
+                });
+            }
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) {
+                try {
+                    movieList = SearchViewModel.addList(response);
+                    page = newPage;
+                    runOnUiThread(() -> {
+                        movieAdapter.notifyDataSetChanged();
+                        loading(false);
+                    });
+                } catch (Exception e) {
+                    runOnUiThread(() -> {
+                        Toast.makeText(SearchActivity.this, "未找到电影", Toast.LENGTH_SHORT).show();
+                        loading(false);
+                    });
+                }
+            }
+        });
+    }
+    private void loading(boolean p){
+        previousButton.setEnabled(!p);
+        nextButton.setEnabled(!p);
+        if(p) progressBar.setVisibility(View.VISIBLE);
+        else progressBar.setVisibility(View.GONE);
     }
 
 }
--- app/src/main/java/com/example/myapplication/network/OnlineSearchUtil.java
+++ app/src/main/java/com/example/myapplication/network/OnlineSearchUtil.java
@@ -1,17 +1,9 @@
 package com.example.myapplication.network;
 
-import android.annotation.SuppressLint;
-import android.util.Log;
-
-import androidx.annotation.NonNull;
-
-import java.io.IOException;
-
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.Response;
 
 public class OnlineSearchUtil {
     private static final String URL = "http://www.omdbapi.com/?apikey=858fc655";
@@ -20,19 +12,7 @@ public class OnlineSearchUtil {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url(query).get().build();
         Call call = client.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                Log.d("--------Fail-------", "onFailure: " + e.toString());
-                callback.onFailure(call, e);
-            }
-
-            @SuppressLint("NotifyDataSetChanged")
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                callback.onResponse(call, response);
-            }
-        });
+        call.enqueue(callback);
     }
 
     public static void searchMovies(String name, Callback callback) {
